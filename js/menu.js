e2680c5477413ae1c48d85730547e541d2c9d93f
Viktoriia Shynkar <viktoriiashynkar2@gmail.com>
2021-10-12 17:32:40 +0300

    Правки:)

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,11 @@ const menuRefs = {
   openMenuBtn: document.querySelector('[data-menu-open-btn]'),
   closeMenuBtn: document.querySelector('[data-menu-close-btn]'),
   menuBackdrop: document.querySelector('[data-menu]'),
-  menuList: document.querySelector('.mobile-menu__list'),
+  // menuList: document.querySelector('.mobile-menu__list'),
 }
 menuRefs.openMenuBtn.addEventListener('click', toggleMenu)
 menuRefs.closeMenuBtn.addEventListener('click', toggleMenu)
-menuRefs.menuList.addEventListener('click', closeMenu)
+// menuRefs.menuList.addEventListener('click', closeMenu)
 
 function toggleMenu() {
   menuRefs.menuBackdrop.classList.toggle('is-hidden')
